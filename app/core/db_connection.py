0aa8900ecf085059b99c40d64ff8e051af2c1d80
jacksonlmr <38480226+jacksonlmr@users.noreply.github.com>
2026-01-28 09:21:45 -0800

    Removed default values for host and port variables in build_engine

--- app/core/db_connection.py
+++ app/core/db_connection.py
@@ -26,8 +26,8 @@ def build_engine() -> Engine:
     try:
         user = quote_plus(os.getenv("DB_USER", "root"))
         password = quote_plus(os.getenv("DB_PASSWORD"))
-        host = os.getenv("DB_HOST", "localhost")
-        port = os.getenv("DB_PORT", "3306")
+        host = os.getenv("DB_HOST")
+        port = os.getenv("DB_PORT")
         db_name = os.getenv("DB_NAME", DATABASE_NAME)
         # Using PyMySQL dialect for compatibility.
         url = f"mysql+pymysql://{user}:{password}@{host}:{port}/{db_name}"
