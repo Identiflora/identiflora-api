526324bc9afc5ec529ba847534d5d3b47b7910ca
Selena Nichols <112664221+nichols-selenium@users.noreply.github.com>
2026-02-22 19:50:20 -0800

    friends main.py
    added friends

--- main.py
+++ main.py
@@ -22,6 +22,9 @@ from app.core.users import get_global_leaderboard, get_count_user, add_user_glob
 from app.db.incorrect_identification import record_incorrect_identification
 from app.db.plant_species import record_plant_species, get_plant_species_url, get_species_id
 
+from app.db.friends import get_friends, add_friend
+from app.models.requests import FriendAddRequest
+
 import logging
 logging.basicConfig(level=logging.INFO)
 
@@ -119,6 +122,16 @@ def google_auth(payload: UserOTPVerifyRequest):
 def get_user_points_router(username: str):
     """Route handler that returns a users global points"""
     return get_user_points(username, engine)
+
+@app.get("/friends")
+def get_friends_router(token_claims: Annotated[dict, Depends(get_current_user)]):
+    user_id = int(token_claims.get("sub"))
+    return get_friends(user_id=user_id, engine=engine)
+
+@app.post("/friends/add")
+def add_friend_router(payload: FriendAddRequest, token_claims: Annotated[dict, Depends(get_current_user)]):
+    user_id = int(token_claims.get("sub"))
+    return add_friend(payload=payload, user_id=user_id, engine=engine)
 
 
 if __name__ == "__main__":
