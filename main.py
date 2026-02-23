7f33f8986717739429a58a474c4815b525fa3a99
Selena Nichols <112664221+nichols-selenium@users.noreply.github.com>
2026-02-22 19:46:43 -0800

    Update main.py

--- main.py
+++ main.py
@@ -123,8 +123,7 @@ def get_user_points_router(username: str):
     """Route handler that returns a users global points"""
     return get_user_points(username, engine)
 
-
- @app.get("/friends")
+@app.get("/friends")
 def get_friends_router(token_claims: Annotated[dict, Depends(get_current_user)]):
     user_id = int(token_claims.get("sub"))
     return get_friends(user_id=user_id, engine=engine)
@@ -133,7 +132,6 @@ def get_friends_router(token_claims: Annotated[dict, Depends(get_current_user)])
 def add_friend_router(payload: FriendAddRequest, token_claims: Annotated[dict, Depends(get_current_user)]):
     user_id = int(token_claims.get("sub"))
     return add_friend(payload=payload, user_id=user_id, engine=engine)
-
 
 if __name__ == "__main__":
     uvicorn.run(
